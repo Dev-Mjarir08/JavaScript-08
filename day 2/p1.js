65f721934ed07119e5b8bcdffe3c19401c9f2155
Dev-Mjarir08 <08mjarir@gmail.com>
2025-08-22 12:00:00 +0530

    update

--- day 2/p1.js
+++ day 2/p1.js
@@ -1,3 +1,5 @@
- console.warn("My Name is Jarir");
-        console.log("I am Learning Javascript");
-        console.info("I am 23 years old");  
+console.warn("My Name is Jarir");
+console.log("I am Learning Javascript");
+console.info("I am 23 years old");
+console.error("I am a Software Engineer");
+console.info("I am from India");
