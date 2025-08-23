3ff9e2a098b8f5d980b47df64011fedb58e1168b
dev-jarir08 <your-email-address>
2025-08-23 12:24:00 +0530

    update

---
+++ day 4 opretors/p1.js
@@ -0,0 +1,9 @@
+// Addition of two numbers
+let a = 10 , b = 20 ;
+console.log("Addition of A + B : " + (a + b));
+let x=50, y=30 ;
+console.log("Subtraction of x-y =" + (x-y));
+console.log("Divison of x/y =" + (x/y));
+console.log("Multiplication of x*y =" + (x*y));
+console.log("Power of x**y =" + (x**y));
+console.log("modulo of x%y =" + (x%y));
