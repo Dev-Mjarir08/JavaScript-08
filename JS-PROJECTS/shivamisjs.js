608c04c3f72e5ad909fcab2a7f7771595fec1fcd
dev-jarir08 <your-email-address>
2025-08-26 13:24:44 +0530

    update

---
+++ JS-PROJECTS/shivamisjs.js
@@ -0,0 +1,8 @@
+let num_1 = 7;
+
+if (num_1 % 2 === 0 || num_1 % 3 === 0 || num_1 % 4 === 0 || num_1 % 5 === 0) {
+    console.log(`Number ${num_1} Is Not A Prime Number`);
+}
+else {
+    console.log(`Number ${num_1} Is A Prime Number`);
+}
