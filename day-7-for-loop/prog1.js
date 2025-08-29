fddb690ca751950a9025ed2f3d0c17ba38142117
dev-jarir08 <your-email-address>
2025-08-29 10:07:55 +0530

    update

--- day-7-for-loop/prog1.js
+++ day-7-for-loop/prog1.js
@@ -2,3 +2,8 @@
 for (let i = 1; i <= 10; i++) {
     console.log(i);
     }
+//using for loop in array
+let arr = [10, 20, 30, 40, 50];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
